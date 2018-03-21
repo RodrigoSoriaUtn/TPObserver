6931f65ec60a32abe63450c302859db305d56052
rodrigosoriautn <vamosrock@hotmail.com>
2018-03-21 20:44:08 -0300

    starter project

---
+++ TPObserver/src/Classes/Car.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Car {
+    
+    private String patent; // ID
+    private int wheelsCuantity;
+    private String color;
+    private String model;
+    private int oilLevel; //0 to 100 (percentage)
+    private int whellPressure;
+    private int waterLevel; // 0 to 100 (percentage)
+    
+    
+    
+    
+}
